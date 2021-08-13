c6189c895426889eeee5aa45235646e646052a69
Haider ali <75686258+Haider-Ali-Dev@users.noreply.github.com>
2021-08-13 19:13:50 +0400

    style changes

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,7 +7,7 @@ class Card extends Component {
     render() {
         return (
             <div className="mw5 mw7-ns center bg-light-blue pa3 ph5-ns br3 pa3 ma2 grow bw2 shadow-5">
-                <h1   className="underline">{this.props.author}</h1>
+                <h1   className="font-bold">{this.props.author}</h1>
                 <div>
                     <p className="i fw8">{this.props.quote}</p>
                     <button onClick={this.props.newQuote} className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded m-3">Next Quote</button>
